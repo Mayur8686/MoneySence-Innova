f22bafdb56c0549ef2b50c08bc5be20e02cf1dd7
Mayur Shinde <mayurs.shinde4212@gmail.com>
2026-07-26 04:14:50 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://moneysence-innova.onrender.com"],
+    allow_origins=["https://money-sence-innova-d4fmad8rz-tech-visioners.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
